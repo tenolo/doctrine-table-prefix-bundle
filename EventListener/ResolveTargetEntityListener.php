99d461c8d01aeb98f62f689127544f2e43925125
Nikita Loges <n.loges@tenolo.de>
2015-03-05 16:55:07 +0100

    change subscriber to event add custom resolve target entity listener

---
+++ EventListener/ResolveTargetEntityListener.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Tenolo\Bundle\DoctrineTablePrefixBundle\EventListener;
+
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+/**
+ * Class ResolveTargetEntityListener
+ * @package Tenolo\Bundle\DoctrineTablePrefixBundle\EventListener
+ * @author Nikita Loges
+ * @company tenolo GbR
+ * @date 05.03.2015
+ */
+class ResolveTargetEntityListener
+{
+
+    /**
+     * @var array
+     */
+    protected $resolveTargetEntities = array();
+
+    /**
+     * Adds a target-entity class name to resolve to a new class name.
+     *
+     * @param string $originalEntity
+     * @param string $newEntity
+     * @param array  $mapping
+     *
+     * @return void
+     */
+    public function addResolveTargetEntity($originalEntity, $newEntity, array $mapping)
+    {
+        $mapping['targetEntity'] = ltrim($newEntity, "\\");
+        $this->resolveTargetEntities[ltrim($originalEntity, "\\")] = $mapping;
+    }
+
+    /**
+     * Processes event and resolves new target entity names.
+     *
+     * @param LoadClassMetadataEventArgs $args
+     *
+     * @return void
+     */
+    public function loadClassMetadata(LoadClassMetadataEventArgs $args)
+    {
+        $cm = $args->getClassMetadata();
+
+        foreach ($cm->associationMappings as $mapping) {
+            if (isset($this->resolveTargetEntities[$mapping['targetEntity']])) {
+                $this->remapAssociation($cm, $mapping);
+            }
+        }
+    }
+
+    /**
+     * @param \Doctrine\ORM\Mapping\ClassMetadataInfo $classMetadata
+     * @param array $mapping
+     *
+     * @return void
+     */
+    protected function remapAssociation($classMetadata, $mapping)
+    {
+        $newMapping = $this->resolveTargetEntities[$mapping['targetEntity']];
+        $newMapping = array_replace_recursive($mapping, $newMapping);
+        $newMapping['fieldName'] = $mapping['fieldName'];
+
+        unset($classMetadata->associationMappings[$mapping['fieldName']]);
+
+        switch ($mapping['type']) {
+            case ClassMetadata::MANY_TO_MANY:
+                $this->remapManyToManyAssociation($classMetadata, $newMapping);
+                break;
+            case ClassMetadata::MANY_TO_ONE:
+                $this->remapManyToOneAssociation($classMetadata, $newMapping);
+                break;
+            case ClassMetadata::ONE_TO_MANY:
+                $this->remapOneToManyAssociation($classMetadata, $newMapping);
+                break;
+            case ClassMetadata::ONE_TO_ONE:
+                $this->remapOneToOneAssociation($classMetadata, $newMapping);
+                break;
+        }
+    }
+
+    /**
+     * @param ClassMetadataInfo $classMetadata
+     * @param $mapping
+     */
+    protected function remapManyToManyAssociation(ClassMetadataInfo $classMetadata, $mapping)
+    {
+        // reset stuff to generate new one
+        unset($mapping['joinTable']['name']);
+        unset($mapping['joinTable']['joinColumns']);
+        unset($mapping['joinTable']['inverseJoinColumns']);
+        unset($mapping['relationToSourceKeyColumns']);
+        unset($mapping['relationToTargetKeyColumns']);
+
+        $classMetadata->mapManyToMany($mapping);
+    }
+
+    /**
+     * @param ClassMetadataInfo $classMetadata
+     * @param $mapping
+     */
+    protected function remapManyToOneAssociation(ClassMetadataInfo $classMetadata, $mapping)
+    {
+        $classMetadata->mapManyToOne($mapping);
+    }
+
+    /**
+     * @param ClassMetadataInfo $classMetadata
+     * @param $mapping
+     */
+    protected function remapOneToManyAssociation(ClassMetadataInfo $classMetadata, $mapping)
+    {
+        $classMetadata->mapOneToMany($mapping);
+    }
+
+    /**
+     * @param ClassMetadataInfo $classMetadata
+     * @param $mapping
+     */
+    protected function remapOneToOneAssociation(ClassMetadataInfo $classMetadata, $mapping)
+    {
+        $classMetadata->mapOneToOne($mapping);
+    }
+}
---
+++ EventListener/TablePrefixListener.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Tenolo\Bundle\DoctrineTablePrefixBundle\EventListener;
+
+use Doctrine\Common\Annotations\Reader;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Tenolo\Bundle\CoreBundle\Service\AbstractService;
+use Tenolo\Bundle\CoreBundle\Util\Crypt;
+use Tenolo\Bundle\CoreBundle\Util\String;
+
+/**
+ * Class TablePrefixListener
+ *
+ * @package Tenolo\Bundle\DoctrineTablePrefixBundle\EventListener
+ * @author Nikita Loges
+ * @company tenolo GbR
+ * @date 03.06.14
+ */
+class TablePrefixListener extends AbstractService
+{
+
+    /**
+     * @var string
+     */
+    protected $prefix = '';
+
+    /**
+     * @var ArrayCollection
+     */
+    protected $loadedClasses;
+
+    /**
+     * @var ArrayCollection
+     */
+    protected $processedAssociation;
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+     * @param $prefix
+     */
+    public function __construct($container, $prefix)
+    {
+        parent::__construct($container);
+
+        $this->prefix = (string)$prefix;
+
+        $this->loadedClasses = new ArrayCollection();
+        $this->processedAssociation = new ArrayCollection();
+    }
+
+    /**
+     * @return string
+     */
+    protected function getPrefix()
+    {
+        return $this->prefix;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    protected function getLoadedClasses()
+    {
+        return $this->loadedClasses;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    protected function getProcessedAssociation()
+    {
+        return $this->processedAssociation;
+    }
+
+    /**
+     * @param LoadClassMetadataEventArgs $args
+     */
+    public function loadClassMetadata(LoadClassMetadataEventArgs $args)
+    {
+        /** @var ClassMetadata $classMetadata */
+        $classMetadata = $args->getClassMetadata();
+
+        $classReflection = $classMetadata->getReflectionClass();
+        $className = $classReflection->getName();
+        $classAnnotation = $this->getReader()->getClassAnnotation($classReflection, 'Tenolo\Bundle\DoctrineTablePrefixBundle\Doctrine\Annotations\Prefix');
+
+        $prefix = $this->prefix;
+        if (!is_null($classAnnotation)) {
+            $tablePrefix = trim($classAnnotation->name);
+
+            if (!empty($tablePrefix)) {
+                $prefix .= $tablePrefix . "_";
+            }
+        }
+
+        // Do not re-apply the prefix in an inheritance hierarchy.
+        if (!$classMetadata->isInheritanceTypeSingleTable() || $classMetadata->isRootEntity()) {
+
+            if ($this->getLoadedClasses()->contains($className)) {
+                return;
+            } else {
+                $this->getLoadedClasses()->add($className);
+            }
+
+            $classMetadata->setPrimaryTable(array(
+                'name' => $prefix . $classMetadata->getTableName()
+            ));
+        }
+
+        foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+            if ($mapping['type'] == ClassMetadataInfo::MANY_TO_MANY && isset($mapping['joinTable']['name'])) {
+                $sourceEntity = $mapping['sourceEntity'];
+                $targetEntity = $mapping['targetEntity'];
+                $serial = Crypt::getHash($sourceEntity . '-' . $targetEntity);
+
+                // set only new associations
+                if (!$this->getProcessedAssociation()->contains($serial)) {
+                    $mappedTableName = $mapping['joinTable']['name'];
+
+                    // remove interface name and save new name
+                    $mapping['joinTable']['name'] = $prefix . $mappedTableName . '_map';
+
+                    // set new association
+                    unset($classMetadata->associationMappings[$mapping['fieldName']]);
+                    $classMetadata->mapManyToMany($mapping);
+
+                    // remember
+                    $this->getProcessedAssociation()->add($serial);
+                }
+            }
+        }
+    }
+}
